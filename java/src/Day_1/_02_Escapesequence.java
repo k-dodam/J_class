a9ee93f1a866fbf9b0f365ea23892a6ec4ba9ed8
minji10 <ch991012@naver.com>
2025-02-04 16:49:32 +0900

    Day_1

---
+++ java/src/Day_1/_02_Escapesequence.java
@@ -0,0 +1,17 @@
+package Day_1;
+
+public class _02_Escapesequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Write\n\nonce!"); // 줄바꿈
+		System.out.println("사과\t배\t오렌지"); // 탭
+		System.out.println("apple\tpear\torange");
+		System.out.println("우리 집 강아지는 \"멍멍\" 짖는다"); // 큰따옴표 문자열 처리
+		// 한줄주석
+		/*
+		 * 여러줄주석
+		 */
+	}
+
+}
---
+++ java/src/Day_1/_03_Print.java
@@ -0,0 +1,24 @@
+package Day_1;
+
+public class _03_Print {
+
+	public static void main(String[] args) {
+		System.out.println("apple");
+		System.out.println("banana");
+		System.out.println("orange");
+		// 큰따옴표로 문자열 출력 : 문자 2개 이상
+		
+		System.out.println("");
+		System.out.println(5);
+		System.out.println(3.14);
+		System.out.println('A');
+		
+		System.out.println("");
+		System.out.println(3+5);
+		System.out.println(3.9+5.72);
+		System.out.println(3.9*5.72);
+		System.out.println(10/3);
+		System.out.println(10/3.0); // 실수인 피연산자 존재 시에 결과값은 실수로 출력
+	}
+
+}
