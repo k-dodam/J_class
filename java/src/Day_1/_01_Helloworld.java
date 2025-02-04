c48dfeefc993a077370d3b302b03f71b22d4cfd1
minji10 <ch991012@naver.com>
2025-02-04 16:15:30 +0900

    Day_1 Test

---
+++ java/src/Day_1/_01_Helloworld.java
@@ -0,0 +1,11 @@
+package Day_1;
+
+public class _01_Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		System.out.print("안녕하세요");
+	}
+
+}
